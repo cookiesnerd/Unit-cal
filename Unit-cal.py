92a0e42d527ef8e5da8d0e50b454b7aabcbf0453
cookiesnerd <ibisarif2008@gmail.com>
2026-01-17 23:16:27 +0100

    added for stone calcu

--- Unit-cal.py
+++ Unit-cal.py
@@ -19,6 +19,10 @@ def weights():
     while True:
         print("\n1. Gram to KG")
         print("2. KG to gram")
+        print("3. Gram to Stone")
+        print("4. Kilogram to stone")
+        print("5.Stone to gram")
+        print("6. Stone to kilogram")
         print("Enter nothing to go back!")
         Weight_choice = input("Which measure would you like to choose: ")
 
@@ -47,6 +51,29 @@ def weights():
         if Weight_choice == "":
             main()
             break
+
+        if Weight_choice == "3":
+            gram = input("How many grams: ")
+            try:
+                gram = float(gram)
+                stone = gram / 6350.29318
+                print(f"{stone} Stone(s)")
+                weights()
+                break
+            except:
+                print("ERROR please input a int or a float not a str!")
+
+        if Weight_choice == "4":
+            kilogram = input("How many Kilograms: ")
+            try:
+                kilogram = float(kilogram)
+                stone = kilogram * 0.157473
+                print(f"{stone} Stone(s)")
+                weights()
+                break
+            except:
+                print("ERROR please input a int or a float not a str!")
+                
                 
 
 main()
